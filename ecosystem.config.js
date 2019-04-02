78ac3ec60ea3031da827fa3324f391c9f3a0f937
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-02 10:32:21 -0500

    completed pm2 setup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-18-224-3-98.us-east-2.compute.amazonaws.com',
+        host: 'ec2-18-222-209-49.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/FEC_Navbar.pem',
         ref: 'origin/master',
-        repo: 'https://github.com/axe-center/AAnderson_Carousel_Service.git',
-        path: '/home/ubuntu/AAnderson_Carousel_Service',
+        repo: 'https://github.com/axe-center/AAnderson_Navbar_carousel_service.git',
+        path: '/home/ubuntu/AAnderson_Navbar_Service',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }
