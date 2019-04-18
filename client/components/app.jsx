693d8d963c8ea86c2a879d9756ec149e624f19a9
jayknyn <hack@jamminglobal.com>
2019-04-18 17:28:04 -0500

    Updating db and seeder file

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,287 +7,270 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Search from './search.jsx';
 import './styles.css';
 
-
-const ec2address = 'http://ec2-18-224-153-75.us-east-2.compute.amazonaws.com'
+const host = 'http://localhost:3005'
 
 const axeTheme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#595959'
-        },
-        textPrimary: {
-            main: '#c9c9c9'
-        },
-        secondary: {
-            main: '#a00000'
-        }
+  palette: {
+    primary: {
+      main: '#595959'
     },
-    typography: {
-        useNextVariants: true,
-      }
+    textPrimary: {
+      main: '#c9c9c9'
+    },
+    secondary: {
+      main: '#a00000'
+    }
+  },
+  typography: {
+    useNextVariants: true,
+    }
 })
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            tags: [],
-            value: false,
-            anchorEl: 16,
-            currentId: 1,
-            currentTag: '',
-            axes: [
-                {
-                    name: 'fighty axe',
-                    tag: 'battle',
-                    image: `https://s3.us-east-2.amazonaws.com/axes/battle+axe/1.+battle-axe.jpg`,
-                    id: 1
-                }
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+      value: false,
+      anchorEl: 16,
+      currentId: 1,
+      currentTag: '',
+      axes: [
+        {
+          name: 'fighty axe',
+          tag: 'battle',
+          image: `https://s3.us-east-2.amazonaws.com/axes/battle+axe/1.+battle-axe.jpg`,
+          id: 1
         }
-        this.handleTabChange = this.handleTabChange.bind(this);
-    };
+      ]
+    }
+    this.handleTabChange = this.handleTabChange.bind(this);
+  };
 
-    componentDidMount() {
-        axios.get(`${ec2address}/api/navbar/products`)
-        .then(results => {
-            const tagArr = [];
-            const tagObj = {};
-            for (let obj of results.data) {
-                if (tagObj[obj.tag] === undefined) {
-                    tagArr.push(obj.tag);
-                    tagObj[obj.tag] = 1;
-                }
-            }
-            this.setState({
-                tags: tagArr,
-                axes: results.data
-            });
-        })
-        .catch(err => {
-            console.error(err);
-        })
-
-
-        window.addEventListener('productId', (e) => {
-            this.setState({
-                currentId: e.detail
-            })
-        })
-    };
-
-
-    handleProductClick(e, id, tag) {
-        // e.preventDefault();
-        if (id && tag) {
-            this.setState({
-                currentId: id,
-                currentTag: tag,
-                value: false
-            }, () => {
-                window.dispatchEvent(new CustomEvent('productId', {"detail": this.state.currentId}))
-            });
-    
-            this.handleBackClick();
-
+  componentDidMount() {
+    axios.get(`${host}/api/navbar/products`)
+    .then(results => {
+      console.log('axios get /api/navbar/products success, results.data.length:', results.data.length, 'results.data[0]: ', results.data[0], results.data[20], results.data[40])
+      const tagArr = [];
+      const tagObj = {};
+      for (let obj of results.data) {
+        if (tagObj[obj.tag] === undefined) {
+          tagArr.push(obj.tag);
+          tagObj[obj.tag] = 1;
         }
+      }
+      this.setState({
+        tags: tagArr,
+        axes: results.data
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    })
+
+    window.addEventListener('productId', (e) => {
+      this.setState({
+        currentId: e.detail
+      })
+    })
+  };
+
+  handleProductClick(e, id, tag) {
+    // e.preventDefault();
+    if (id && tag) {
+      this.setState({
+        currentId: id,
+        currentTag: tag,
+        value: false
+      }, () => {
+        window.dispatchEvent(new CustomEvent('productId', {"detail": this.state.currentId}))
+      });
+      this.handleBackClick();
+    }
+  }
+
+  findallAxesFromTag(tag) {
+    const axes = this.state.axes
+    const taggedAxes = [];
+    for (let i =0 ; i < axes.length; i++) {
+      if (axes[i].tag === tag) {
+        taggedAxes.push(axes[i]);
+      }
+    }
+    return taggedAxes
+  };
+
+  handleTabChange(e,evalue) {
+    e.preventDefault();
+    let val = 10;
+    if (evalue === 'two') {
+      val = 10 + 0;
+    } else if (evalue === 'three') {
+      val = val + 11.5;
+    } else if (evalue === 'four') {
+      val = val + 23;
+    } else if (evalue === 'five') {
+      val = val + 34.5;
     }
 
-    findallAxesFromTag(tag) {
-        const axes = this.state.axes
-        const taggedAxes = [];
-        for (let i =0 ; i < axes.length; i++) {
-            if (axes[i].tag === tag) {
-                taggedAxes.push(axes[i]);
-            }
-        }
+    this.setState({
+      value: evalue,
+      anchorEl: val
+    })
+  }
 
-        return taggedAxes
-    };
+  handleTabLeave(e) {
+    e.preventDefault();
+    this.setState({
+      value: false
+    })
+  }
 
-    handleTabChange(e,evalue) {
-        e.preventDefault();
-        let val = 10;
-        if (evalue === 'two') {
-            val = 10 + 0;
-        } else if (evalue === 'three') {
-            val = val + 11.5;
-        } else if (evalue === 'four') {
-            val = val + 23;
-        } else if (evalue === 'five') {
-            val = val + 34.5;
-        }
+  handleContactClick(e) {
+    e.preventDefault();
 
-        this.setState({
-            value: evalue,
-            anchorEl: val
-        })
+    let about = document.getElementById('aboutInfo');
+    let phil = document.getElementById('philInfo');
+    if (about) {
+      about.remove();
+    }
+    if (phil) {
+      phil.remove();
+    }
+    if (!document.getElementById('contactInfo')) {
+      let body = document.getElementsByTagName('body');
+      let contactDiv = document.createElement('div');
+      let products = document.getElementById('products');
+  
+      contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
+        <h1>Hello, please use the below information to contact us</h1> 
+        <p class = 'contactInfo' >Phone: 555-123-4567</p>
+        <p class = 'contactInfo'>Email: axesssss@axesRus.org</p>
+        <p class = 'contactInfo' >pigeon: over yonder</p>
+      </div>`
+  
+      contactDiv.id = 'contactInfo'
+      body[0].insertBefore(contactDiv,products);
+    }
+    document.getElementById('carousel').style.visibility = 'hidden' 
+    document.getElementById('reviews').style.visibility = 'hidden' 
+    document.getElementById('graph').style.visibility = 'hidden' 
+    document.getElementById('products').style.visibility = 'hidden' 
+  }
+
+  handleAboutClick(e) {
+    e.preventDefault();
+    let contact = document.getElementById('contactInfo');
+    let phil = document.getElementById('philInfo');
+    if (contact) {
+      contact.remove();
+    }
+    if (phil) {
+      phil.remove();
     }
 
-    handleTabLeave(e) {
-        e.preventDefault();
-        this.setState({
-            value: false
-        })
+    if (!document.getElementById('aboutInfo')) {
+      let body = document.getElementsByTagName('body');
+      let contactDiv = document.createElement('div');
+      let products = document.getElementById('products');
+  
+      contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
+        <h1>Hello, we sell axes</h1> 
+      </div>`
+  
+      contactDiv.id = 'aboutInfo'
+      body[0].insertBefore(contactDiv,products);
+    }
+    document.getElementById('carousel').style.visibility = 'hidden' 
+    document.getElementById('reviews').style.visibility = 'hidden' 
+    document.getElementById('graph').style.visibility = 'hidden' 
+    document.getElementById('products').style.visibility = 'hidden' 
+  }
+
+  handlePhilClick(e) {
+    e.preventDefault();
+    let contact = document.getElementById('contactInfo');
+    let about = document.getElementById('aboutInfo');
+    if (contact) {
+      contact.remove();
+    }
+    if (about) {
+      about.remove();
     }
 
-    handleContactClick(e) {
-        e.preventDefault();
-
-        let about = document.getElementById('aboutInfo');
-        let phil = document.getElementById('philInfo');
-        if (about) {
-            about.remove();
-        }
-        if (phil) {
-            phil.remove();
-        }
-        if (!document.getElementById('contactInfo')) {
-            let body = document.getElementsByTagName('body');
-            let contactDiv = document.createElement('div');
-            let products = document.getElementById('products');
-    
-            contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
-                <h1>Hello, please use the below information to contact us</h1> 
-                <p class = 'contactInfo' >Phone: 555-123-4567</p>
-                <p class = 'contactInfo'>Email: axesssss@axesRus.org</p>
-                <p class = 'contactInfo' >pigeon: over yonder</p>
-            </div>`
-    
-            contactDiv.id = 'contactInfo'
-            body[0].insertBefore(contactDiv,products);
-        }
-        document.getElementById('carousel').style.visibility = 'hidden' 
-        document.getElementById('reviews').style.visibility = 'hidden' 
-        document.getElementById('graph').style.visibility = 'hidden' 
-        document.getElementById('products').style.visibility = 'hidden' 
-        
-
+    if (!document.getElementById('philInfo')) {
+      let body = document.getElementsByTagName('body');
+      let contactDiv = document.createElement('div');
+      let products = document.getElementById('products');
+  
+      contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
+        <h1>Hello, we are the prime donators to LIN, or Larpers In Need</h1> 
+      </div>`
+  
+      contactDiv.id = 'philInfo'
+      body[0].insertBefore(contactDiv,products);
     }
+    document.getElementById('carousel').style.visibility = 'hidden' 
+    document.getElementById('reviews').style.visibility = 'hidden' 
+    document.getElementById('graph').style.visibility = 'hidden' 
+    document.getElementById('products').style.visibility = 'hidden' 
+  }
 
-    handleAboutClick(e) {
-        e.preventDefault();
-
-        let contact = document.getElementById('contactInfo');
-        let phil = document.getElementById('philInfo');
-        if (contact) {
-            contact.remove();
-        }
-        if (phil) {
-            phil.remove();
-        }
-
-        if (!document.getElementById('aboutInfo')) {
-            let body = document.getElementsByTagName('body');
-            let contactDiv = document.createElement('div');
-            let products = document.getElementById('products');
-    
-            contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
-                <h1>Hello, we sell axes</h1> 
-            </div>`
-    
-            contactDiv.id = 'aboutInfo'
-            body[0].insertBefore(contactDiv,products);
-        }
-        document.getElementById('carousel').style.visibility = 'hidden' 
-        document.getElementById('reviews').style.visibility = 'hidden' 
-        document.getElementById('graph').style.visibility = 'hidden' 
-        document.getElementById('products').style.visibility = 'hidden' 
-
+  handleBackClick() {
+    // e.preventDefault();
+    let contact = document.getElementById('contactInfo');
+    let about = document.getElementById('aboutInfo');
+    let phil = document.getElementById('philInfo');
+    if (contact) {
+      contact.remove();
     }
-
-    handlePhilClick(e) {
-        e.preventDefault();
-
-        let contact = document.getElementById('contactInfo');
-        let about = document.getElementById('aboutInfo');
-        if (contact) {
-            contact.remove();
-        }
-        if (about) {
-            about.remove();
-        }
-
-        if (!document.getElementById('philInfo')) {
-            let body = document.getElementsByTagName('body');
-            let contactDiv = document.createElement('div');
-            let products = document.getElementById('products');
-    
-            contactDiv.innerHTML = `<div class = 'contactInfo contactInfoDiv'>
-                <h1>Hello, we are the prime donators to LIN, or Larpers In Need</h1> 
-            </div>`
-    
-            contactDiv.id = 'philInfo'
-            body[0].insertBefore(contactDiv,products);
-        }
-        document.getElementById('carousel').style.visibility = 'hidden' 
-        document.getElementById('reviews').style.visibility = 'hidden' 
-        document.getElementById('graph').style.visibility = 'hidden' 
-        document.getElementById('products').style.visibility = 'hidden' 
-
+    if (about) {
+      about.remove();
     }
-
-    handleBackClick() {
-        // e.preventDefault();
-        let contact = document.getElementById('contactInfo');
-        let about = document.getElementById('aboutInfo');
-        let phil = document.getElementById('philInfo');
-        if (contact) {
-            contact.remove();
-        }
-        if (about) {
-            about.remove();
-        }
-        if (phil) {
-            phil.remove();
-        }
-        document.getElementById('carousel').style.visibility = 'visible' 
-        document.getElementById('reviews').style.visibility = 'visible' 
-        document.getElementById('graph').style.visibility = 'visible' 
-        document.getElementById('products').style.visibility = 'visible' 
+    if (phil) {
+      phil.remove();
     }
+    document.getElementById('carousel').style.visibility = 'visible' 
+    document.getElementById('reviews').style.visibility = 'visible' 
+    document.getElementById('graph').style.visibility = 'visible' 
+    document.getElementById('products').style.visibility = 'visible' 
+  }
 
+  render() {
+    let tab = <div></div>
 
-    render() {
-        
+    if (this.state.value === false) {
+      tab = <div></div>
+    } else if (this.state.value === 'two') {
+      tab = <div id = 'tabContainer' style = {{marginLeft: `${this.state.anchorEl}vw`}} onClick = {(e) => {this.handleBackClick(e)}}>
+            {this.state.tags.map((tag, index) => {
+              return <Tag tag = {tag} length = {this.state.tags.length} axes = {this.findallAxesFromTag(tag)} 
+              key = {index} handleProductClick = {this.handleProductClick.bind(this)}/> 
+            })}
+        </div>
+    } 
 
-        let tab = <div></div>
-
-        if (this.state.value === false) {
-            tab = <div></div>
-        } else if (this.state.value === 'two') {
-            tab = <div id = 'tabContainer' style = {{marginLeft: `${this.state.anchorEl}vw`}} onClick = {(e) => {this.handleBackClick(e)}}>
-                        {this.state.tags.map((tag, index) => {
-                            return <Tag tag = {tag} length = {this.state.tags.length} axes = {this.findallAxesFromTag(tag)} 
-                            key = {index} handleProductClick = {this.handleProductClick.bind(this)}/> 
-                        })}
-                </div>
-        } 
-
-        return (
-        <MuiThemeProvider theme = {axeTheme}>
-            <AppBar position = 'static' style = {{height:'10vh',display: 'flex', flexDirection: 'row'}}>
-                
-                <Typography variant="h6" color="textPrimary" 
-                style = {{color: '#c9c9c9', marginLeft: '10vw'}} 
-                onClick = {(e) => {this.handleBackClick(e)}}><img id = 'Logo' src = 'https://s3.us-east-2.amazonaws.com/axes/axe.png'></img></Typography>
-                <Search axesArray = {this.state.axes} handleProductClick = {this.handleProductClick.bind(this)} 
-                handleBackClick = {this.handleBackClick.bind(this)}/>
-            </AppBar>
-            <div onMouseLeave = {this.handleTabLeave.bind(this)}>
-            <Tabs style = {{backgroundColor: '#c9c9c9'}} value = {this.state.value} onChange = {this.handleTabChange.bind(this)} >
-                
-                <Tab value = 'two' icon = {<MenuIcon/>} onMouseEnter = {(e) => this.handleTabChange(e,'two')} style = {{width: '11.5vw', marginLeft: '10vw', color: '#a00000'}}/>
-                <Tab value = 'three' label = 'Contact us' onMouseEnter = {(e) => this.handleTabChange(e,'three')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handleContactClick(e)}}/>
-                <Tab value = 'four' label = 'About us' onMouseEnter = {(e) => this.handleTabChange(e,'four')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handleAboutClick(e)}}/>
-                <Tab value = 'five' label = 'Philanthropy' onMouseEnter = {(e) => this.handleTabChange(e,'five')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handlePhilClick(e)}}/>
-            </Tabs>
-            {tab}
-            </div>
-        </MuiThemeProvider>
-        )
-    }
+    return (
+      <MuiThemeProvider theme = {axeTheme}>
+        <AppBar position = 'static' style = {{display: 'flex', flexDirection: 'row'}}>
+          <Typography variant="h6" color="textPrimary" 
+          style = {{color: '#c9c9c9', marginLeft: '10vw'}} 
+          onClick = {(e) => {this.handleBackClick(e)}}><img id = 'Logo' src = 'https://s3.us-east-2.amazonaws.com/axes/axe.png'></img></Typography>
+          <Search axesArray = {this.state.axes} handleProductClick = {this.handleProductClick.bind(this)} 
+          handleBackClick = {this.handleBackClick.bind(this)}/>
+        </AppBar>
+        <div onMouseLeave = {this.handleTabLeave.bind(this)}>
+        <Tabs style = {{backgroundColor: '#c9c9c9'}} value = {this.state.value} onChange = {this.handleTabChange.bind(this)} >
+          <Tab value = 'two' icon = {<MenuIcon/>} onMouseEnter = {(e) => this.handleTabChange(e,'two')} style = {{width: '11.5vw', marginLeft: '10vw', color: '#a00000'}}/>
+          <Tab value = 'three' label = 'Contact us' onMouseEnter = {(e) => this.handleTabChange(e,'three')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handleContactClick(e)}}/>
+          <Tab value = 'four' label = 'About us' onMouseEnter = {(e) => this.handleTabChange(e,'four')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handleAboutClick(e)}}/>
+          <Tab value = 'five' label = 'Philanthropy' onMouseEnter = {(e) => this.handleTabChange(e,'five')} style = {{width: '11.5vw'}} onClick = {(e) => {this.handlePhilClick(e)}}/>
+        </Tabs>
+        {tab}
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -4,98 +4,96 @@ const cors = require('cors');
 const AWS = require('aws-sdk');
 const randomDescription = require('./random_text.js');
 const db = require('../database/db.js');
-
-AWS.config.update({
-    accessKeyId: process.env.S3AccessKey,
-    secretAccessKey: process.env.S3SecretKey,
-    region: 'us-east-2'
+app.listen(process.env.PORT || 3005, (err, res) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Server running in aisle 3005');
+  }
 });
-
-const s3 = new AWS.S3();
 
 app.use(express.json({urlencoded: true}));
 app.use(cors());
 app.use(express.static('dist'))
 
+AWS.config.update({
+  accessKeyId: process.env.S3AccessKey,
+  secretAccessKey: process.env.S3SecretKey,
+  region: 'us-east-2'
+});
+
+const s3 = new AWS.S3();
+
 const params = {
-    Bucket: 'axes'
+  Bucket: 'axes'
 }
 
 app.get('/api/navbar/products', (req, res) => {
-    db.getAll((err, data) => {
-        if (err) {
-            console.log(err);
-            res.end()
-        } else {
-            res.send(data);
-        }
-    })
+  db.getAll((err, data) => {
+    if (err) {
+      console.log('db getAll error:', err);
+      res.end('db getAll error')
+    } else {
+      console.log('db getAll success, data.length', data.length)
+      res.send(data);
+    }
+  })
 });
 
+app.post('/seed', (req, res) => {
+  s3.listObjects(params, function (err, data) {
+    if(err) {
+      throw err;
+    } else {
+      const urlArr = [];
+      for (let i = 0; i < data.Contents.length; i++) {
+        let path = data.Contents[i].Key//.replace(' ','+');
+        urlArr.push('https://s3.us-east-2.amazonaws.com/axes/'+path)
+      }
+      const tags = ['battle','war','fireman','hatchet','lumber','halberd','boarding','throwing']
+
+      const randomData = randomDescription.randomData.filter(value => {
+        return value !== '';
+      })
+
+      for (let j = 0; j < randomDescription.randomNames.length; j++) {
+        const img1 = Math.floor(Math.random()*(urlArr.length))
+        const img2 = Math.floor(Math.random()*(urlArr.length))
+        const img3 = Math.floor(Math.random()*(urlArr.length))
+        const tagIndex = Math.floor(Math.random()*(tags.length))
+
+        const productPrice = Math.floor(Math.random()*2000) + 500
+        const productImages = [urlArr[img1], urlArr[img2], urlArr[img3]];
+        const productName = randomDescription.randomNames[j];
+        const productDescription = randomData[j];
+        const productTag = tags[tagIndex]
+        const productId = j+1
+        
+        db.save(productId, productName, productImages, productDescription, productPrice, productTag)
+      }
+    }
+    res.end();
+  });
+})
 
 // app.post('/transfer', (req, res) => {
-//     db.getAll((err, data) => {
-//         if (err) {
-//             console.log(err);
-//             res.end();
-//         } else {
-//             res.send(JSON.stringify(data));
-//         }
-//     })
+//   db.getAll((err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end();
+//     } else {
+//       res.send(JSON.stringify(data));
+//     }
+//   })
 // })
 
 // app.post('/receiveTransfer', (req, res) => {
-//     // console.log(req.body);
-//     console.log(req.body[0].productId+100);
-//     const dataArr = req.body
-//     for (let i = 0; i < dataArr.length; i++) {
-//         db.save(dataArr[i].productId, dataArr[i].name, dataArr[i].images, dataArr[i].description, dataArr[i].price, dataArr[i].tag)
-//     }
+//   // console.log(req.body);
+//   console.log(req.body[0].productId+100);
+//   const dataArr = req.body
+//   for (let i = 0; i < dataArr.length; i++) {
+//     db.save(dataArr[i].productId, dataArr[i].name, dataArr[i].images, dataArr[i].description, dataArr[i].price, dataArr[i].tag)
+//   }
 
-//     res.end();
+//   res.end();
 // })
-
-app.post('/seed', (req, res) => {
-    s3.listObjects(params, function (err, data) {
-        if(err) {
-            throw err;
-        } else {
-            const urlArr = [];
-            for (let i = 0; i < data.Contents.length; i++) {
-                let path = data.Contents[i].Key//.replace(' ','+');
-                urlArr.push('https://s3.us-east-2.amazonaws.com/axes/'+path)
-            }
-            const tags = ['battle','war','fireman','hatchet','lumber','halberd','boarding','throwing']
-
-            const randomData = randomDescription.randomData.filter(value => {
-                return value !== '';
-            })
-
-            for (let j = 0; j < randomDescription.randomNames.length; j++) {
-                const img1 = Math.floor(Math.random()*(urlArr.length))
-                const img2 = Math.floor(Math.random()*(urlArr.length))
-                const img3 = Math.floor(Math.random()*(urlArr.length))
-                const tagIndex = Math.floor(Math.random()*(tags.length))
-
-                const productPrice = Math.floor(Math.random()*2000) + 500
-                const productImages = [urlArr[img1], urlArr[img2], urlArr[img3]];
-                const productName = randomDescription.randomNames[j];
-                const productDescription = randomData[j];
-                const productTag = tags[tagIndex]
-                const productId = j+1
-                
-                db.save(productId, productName, productImages, productDescription, productPrice, productTag)
-            }
-        }
-
-        res.end();
-    });
-})
-
-app.listen(process.env.PORT || 3005, (err, res) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('we in dis');
-    }
-});
