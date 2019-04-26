7a0bb92aa21a130a1725db362f621d25b6997571
jayknyn <hack@jamminglobal.com>
2019-04-25 19:56:55 -0500

    updated for 2 ecosystems

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,15 @@ module.exports = {
         repo: 'https://github.com/ten-7/jk-navbar.git',
         path: '/home/ubuntu/navbar',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }, 
+      production2: {
+        user: 'ubuntu',
+        host: '18.218.238.147',
+        key: '~/.ssh/jksdc.pem',
+        ref: 'origin/master',
+        repo: 'https://github.com/ten-7/jk-navbar.git',
+        path: '/home/ubuntu/navbar',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }
   }
